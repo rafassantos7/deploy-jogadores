573d02e0a3145fc6461abea5176ccd3476b805cc
Rafael Vitor <160610194+rafassantos7@users.noreply.github.com>
2025-06-02 09:36:02 -0300

    Delete api directory

--- api/src/main/java/com/example/aula/exception/NumeroCamisaJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.aula.exception;
-
-public class NumeroCamisaJaCadastradoException extends RuntimeException {
-    public NumeroCamisaJaCadastradoException(String message) {
-        super(message);
-    }
-}
